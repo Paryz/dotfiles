c3d94c4cec01a77424f3c7dd386a2b959b521dd3
Kamil Zielinski <paryzowy@gmail.com>
2026-02-05 15:46:09 +0100

    Clean up init.lua by removing redundant code
    Refactor LSP and Treesitter configurations, remove unused keymaps and comments.

--- init.lua
+++ init.lua
@@ -10,7 +10,6 @@ plug("williamboman/mason.nvim")
 plug("williamboman/mason-lspconfig.nvim")
 
 --LSP
-plug("neoclide/coc.nvim", {["branch"] = "release"})
 plug("neovim/nvim-lspconfig")
 plug("lukas-reineke/lsp-format.nvim")
 
@@ -56,7 +55,15 @@ vim.opt.termguicolors = true
 vim.cmd.colorscheme("gruvbox")
 
 -- Disable side scrolling
-vim.opt.wrap = true
+vim.opt.wrap = true        -- Enable line wrapping
+vim.opt.sidescroll = 0     -- Disable horizontal scrolling
+vim.opt.linebreak = true      -- Wrap at word boundaries, not mid-word
+vim.opt.breakindent = true    -- Preserve indentation in wrapped lines
+vim.opt.colorcolumn = "80"    -- Show a visual guide at column 80
+
+-- Disable horizontal scrolling via trackpad/mouse
+vim.keymap.set({'n', 'i', 'v'}, '<ScrollWheelLeft>', '<Nop>', { noremap = true })
+vim.keymap.set({'n', 'i', 'v'}, '<ScrollWheelRight>', '<Nop>', { noremap = true })
 
 -- Map Leader to space
 vim.keymap.set("n", "<Space>", "<Nop>", { silent = true, remap = false })
@@ -120,9 +127,6 @@ vim.opt.shortmess:append({ I = true })
 
 -- use line numbers
 vim.o.number = true
-
--- Disable word wrap
-vim.o.wrap = false
 
 -- Do not highlight all search hits
 vim.o.hlsearch = false
@@ -363,176 +367,3 @@ return {
     on_dir(root_dir)
   end,
 }
-
--- keymaps to yank file name
-vim.keymap.set('n', '<Leader>yff', ':let @+ = expand("%")<cr>', { silent = true })
-vim.keymap.set('n', '<Leader>yf', ':let @+ = expand("%:t")<cr>', { silent = true })
-
--- Shortcut to open workingMemory.txt
-vim.keymap.set('n', '<Leader>wm', function() vim.cmd.edit('~/workingMemory.txt') end, { silent = false })
-
--- Try to prevent bad habits like using the arrow keys for movement. This is
--- not the only possible bad habit. For example, holding down the h/j/k/l keys
--- for movement, rather than using more efficient movement commands, is also a
--- bad habit. The former is enforceable through a .vimrc, while we don't know
--- how to prevent the latter.
-vim.keymap.set('n', '<Left>',  ':echoe "Use h"<CR>')
-vim.keymap.set('n', '<Right>', ':echoe "Use l"<CR>')
-vim.keymap.set('n', '<Up>',    ':echoe "Use k"<CR>')
-vim.keymap.set('n', '<Down>',  ':echoe "Use j"<CR>')
-
--- Shortcuts to navigate the buffer list
-vim.keymap.set('n', ']b', vim.cmd.bnext, { noremap = true, silent = true })
-vim.keymap.set('n', '[b', vim.cmd.bprev, { noremap = true, silent = true })
-
--- Open the altfile
-vim.keymap.set('n', '[a', function() vim.cmd.edit("#") end, { noremap = true, silent = true })
-
--- Shortcuts for navigating quickfix list
-vim.keymap.set('n', ']q', ":cnext<CR>", { noremap = true, silent = true })
-vim.keymap.set('n', '[q', ":cprev<CR>", { noremap = true, silent = true })
-
--- Git conflict marker shortcuts
-vim.keymap.set('n', '<Leader><', '/<<<<<<<<CR>', { silent = true })
-vim.keymap.set('n', '<Leader>>', '/>>>>>>><CR>', { silent = true })
-vim.keymap.set('n', '<Leader>=', '/=======<CR>', { silent = true })
-
--- from https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
--- Multipurpose tab key
-function InsertTab()
-	if (has_words_before()) then
-		-- There's an identifier before the cursor, so complete the identifier.
-		return "<c-p>"
-    end
-
-    return "<tab>"
-end
-
-vim.keymap.set('i', '<tab>', InsertTab, { expr = true })
-vim.keymap.set('i', '<s-tab>', '<c-n>', { noremap = true })
-
--- Treesitter config
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { "elixir", "java", "elm", "rust", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
-
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
-
-  -- List of parsers to ignore installing (or "all")
-  ignore_install = { "javascript" },
-
-  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-  highlight = {
-    enable = true,
-
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- list of language that will be disabled
-    disable = { "c" },
-    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-    disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-            return true
-        end
-    end,
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
-}
-
--- Mason config
-require("mason").setup()
-require("mason-lspconfig").setup()
-
--- LSP config
-local nvim_lsp = require('lspconfig')
-
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-    local opts = { noremap=true, silent=true }
-    -- Mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    vim.keymap.set('n', '<Leader>d', vim.diagnostic.open_float, opts)
-    vim.keymap.set('n', '[w', vim.diagnostic.goto_prev, opts)
-    vim.keymap.set('n', ']w', vim.diagnostic.goto_next, opts)
-    vim.keymap.set('n', '<Leader>q', vim.diagnostic.setqflist, opts)
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    -- vim.keymap.set('n', '<Leader>r', vim.lsp.buf.rename, opts)
-    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-
-    -- always show a sign column of width 1
-    vim.wo.signcolumn = "yes:1"
-
-    -- format on save if available
-    if client.server_capabilities.documentFormattingProvider then
-        -- use autocommand group so re-attach will reset
-        local group_id = vim.api.nvim_create_augroup("Format", { clear = true })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = group_id,
-            pattern = "<buffer>",
-            callback = function() vim.lsp.buf.format({bufnr = bufnr}) end
-        })
-    end
-end
-
-local servers = { "elixirls", "jdtls" } -- any other servers you want
-
-for _, lsp in pairs(servers) do
-    local config = {
-        on_attach = on_attach,
-        flags = {
-            debounce_text_changes = 150,
-        },
-    }
-
-    nvim_lsp[lsp].setup(config)
-end
-
-vim.diagnostic.config({
-  virtual_text = false,
-  underline = { severity = vim.diagnostic.severity.ERROR },
-  update_in_insert = false,
-  signs = true,
-})
-
--- always show
-local signs = { Error = ">", Warn = "W", Hint = "H", Info = "I" }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
-
-
--- LSP format config
-require("lsp-format").setup {}
-require("lspconfig").gopls.setup { on_attach = require("lsp-format").on_attach }
